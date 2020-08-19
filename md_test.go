cc6b96dd451ee95f4f0975e332f0ee94370aebc6
Shi Han NG <shihanng@gmail.com>
2020-08-19 19:42:20 +0900

    Add printNodes for debugging

--- md_test.go
+++ md_test.go
@@ -2,16 +2,22 @@ package md
 
 import (
 	"bytes"
+	"flag"
+	"fmt"
 	"io/ioutil"
+	"strings"
 	"testing"
 
 	"github.com/sebdah/goldie/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/ast"
 	"github.com/yuin/goldmark/renderer"
 	"github.com/yuin/goldmark/text"
 	"github.com/yuin/goldmark/util"
 )
+
+var debug = flag.Bool("debug", false, "Debug with ast.Walk")
 
 func TestRenderer(t *testing.T) {
 	testCases := []string{
@@ -29,6 +35,8 @@ func TestRenderer(t *testing.T) {
 			reader := text.NewReader(input)
 			node := goldmark.DefaultParser().Parse(reader)
 			r := renderer.NewRenderer(renderer.WithNodeRenderers(util.Prioritized(&Renderer{}, 1000)))
+
+			printNodes(node, input)
 
 			assert.NoError(t, r.Render(&buf, input, node))
 
@@ -55,10 +63,34 @@ func TestReRenderer(t *testing.T) {
 			node := goldmark.DefaultParser().Parse(reader)
 			r := renderer.NewRenderer(renderer.WithNodeRenderers(util.Prioritized(&Renderer{}, 1000)))
 
+			printNodes(node, input)
+
 			assert.NoError(t, r.Render(&buf, input, node))
 
 			g := goldie.New(t)
 			g.Assert(t, tc, buf.Bytes())
 		})
+	}
+}
+
+func printNodes(node ast.Node, source []byte) {
+	if !*debug {
+		return
+	}
+
+	space := 0
+	if err := ast.Walk(node, func(node ast.Node, entering bool) (ast.WalkStatus, error) {
+		if entering {
+			text := node.Text(source)
+			fmt.Printf("%s%s: '%.10s'\n", strings.Repeat(" ", space), node.Kind(), text)
+			space++
+		} else {
+			space--
+			fmt.Printf("%s%s\n", strings.Repeat(" ", space), node.Kind())
+		}
+
+		return ast.WalkContinue, nil
+	}); err != nil {
+		fmt.Println(err)
 	}
 }
